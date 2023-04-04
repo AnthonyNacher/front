3cda1a9ab2042feccd6564347b5911fbb4d1f99f
AnthonyNacher <nacheranthony@gmail.com>
2023-04-05 00:15:17 +0200

    Added basic values and sliders for entities' status

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,13 +1,70 @@
 <template>
   <div class="flex flex-col gap-5">
     <span class="text-indigo-600 font-bold text-2xl">Dashboard</span>
-    <ul>
-      <li
-        v-for="entity in entities"
-        :key="entity.id">
-        {{ entity.name }}
-      </li>
-    </ul>
+    <table class="table-auto">
+      <thead>
+        <tr>
+          <th>Nom de l'entité</th>
+          <th>Type</th>
+          <th>Valeur</th>
+          <th>Statut</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          class="text-center"
+          v-for="entity in entities"
+          :key="entity.id">
+          <td>
+            {{ entity.name }}
+          </td>
+          <td>
+            {{ entity.type }}
+          </td>
+          <td>
+            {{ entity.value }}
+          </td>
+          <td>
+            <div v-if="entity.status=='on'">
+              <label class="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  class="sr-only peer"
+                  checked 
+                  disabled>
+                <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              
+              </label>
+            </div>
+            <div v-else-if="entity.status=='off'">
+              <label class="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  class="sr-only peer"
+                  disabled>
+                <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              
+              </label>
+            </div>
+            
+            <div v-else>
+              <label class="relative inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  value=""
+                  class="sr-only peer"
+                  disabled>
+                <div class="w-11 h-6 bg-red-200 rounded-full peer dark:bg-red-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              
+              </label>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    
   </div>
 </template>
 
@@ -45,3 +102,7 @@ export default {
   } 
 }
 </script>
+
+<style scoped>
+
+</style>
