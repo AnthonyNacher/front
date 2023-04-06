57bbc0d0e33d4fdf8421282069ef816aa6527392
AnthonyNacher <nacheranthony@gmail.com>
2023-04-06 22:06:02 +0200

    Added new messages for UX feedbacks

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -1,7 +1,66 @@
 <template>
   <div class="flex flex-col gap-5">
-    
-    <EntityArray />
+    <div
+      v-for="message of messages"
+      v-bind:key="message">
+
+      <div
+        class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+        role="alert"
+        v-if="message.type === 'alert'">
+        <strong class="font-bold"> {{ message.header }}</strong>
+        <span class="block sm:inline"> {{ message.content }}</span>
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg
+            class="fill-current h-6 w-6 text-red-500"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            @click="deleteMessage(message.id)">
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </span>
+      </div>
+      <div
+        class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+        role="success"
+        v-else-if="message.type === 'success'">
+        <strong class="font-bold"> {{ message.header }}</strong>
+        <span class="block sm:inline"> {{ message.content }}</span>
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg
+            class="fill-current h-6 w-6 text-green-500"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            @click="deleteMessage(message.id)">
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </span>
+      </div>
+      <div
+        class="bg-blue-100 border border-blue-400 text-blue-700 px-4 py-3 rounded relative"
+        role="information"
+        v-else-if="message.type === 'information'">
+
+        <strong class="font-bold"> {{ message.header }}</strong>
+        <span class="block sm:inline"> {{ message.content }}</span>
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg
+            class="fill-current h-6 w-6 text-blue-500"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            @click="deleteMessage(message.id)">
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </span>
+      </div>
+    </div>
+    <EntityCreation />
     <span class="text-orange-600 font-bold text-2xl text-center">Tableau de bord APERTURE SCIENCE</span>
     <table class="table-auto">
       <thead>
@@ -26,35 +85,37 @@
           </td>
           <td>
             {{ Number(entity.value).toLocaleString() }} €
-          </td> 
+          </td>
           <td>
             <!-- BLUE SLIDER - ON -->
-            <div v-if="entity.status=='on'">
+            <div v-if="entity.status == 'on'">
               <label class="relative inline-flex items-center cursor-pointer">
                 <input
                   type="checkbox"
                   value=""
                   class="sr-only peer"
-                  checked 
+                  checked
                   disabled>
-                <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              
+                <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                </div>
+
               </label>
             </div>
-            
+
             <!-- GREY SLIDER - OFF -->
-            <div v-else-if="entity.status=='off'">
+            <div v-else-if="entity.status == 'off'">
               <label class="relative inline-flex items-center cursor-pointer">
                 <input
                   type="checkbox"
                   value=""
                   class="sr-only peer"
                   disabled>
-                <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              
+                <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                </div>
+
               </label>
             </div>
-            
+
             <!-- RED SLIDER - UNAVAVAILBLE -->
             <div v-else>
               <label class="relative inline-flex items-center cursor-pointer">
@@ -63,8 +124,9 @@
                   value=""
                   class="sr-only peer"
                   disabled>
-                <div class="w-6 h-6 bg-red-200 rounded-full peer dark:bg-red-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-              
+                <div class="w-6 h-6 bg-red-200 rounded-full peer dark:bg-red-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                </div>
+
               </label>
             </div>
           </td>
@@ -87,13 +149,13 @@
         </tr>
       </tbody>
     </table>
-    
+
   </div>
 </template>
 
 <script>
 import coreApi from "@/providers/core-api"
-import EntityArray from "@/components/forms/EntityArray.vue"
+import EntityCreation from "@/components/forms/EntityCreation.vue"
 
 export default {
   name: "Dashboard",
@@ -104,11 +166,13 @@ export default {
     return {
       entities: [],
       isLoading: false,
-      isError: false 
+      isError: false,
+      messages: {},
+      message_count: 0
     }
   },
-  components: { EntityArray },
-  methods: { 
+  components: { EntityCreation },
+  methods: {
     getEntities() {
       this.isLoading = true
 
@@ -124,8 +188,7 @@ export default {
           this.isLoading = false
         })
     },
-    getFrenchTypeDisplay(valueToTranslate)
-    {
+    getFrenchTypeDisplay(valueToTranslate) {
       let translations = {
         "sensor": "détecteur",
         "light": "lumière",
@@ -137,13 +200,13 @@ export default {
         return translations[valueToTranslate].toUpperCase()
       else
         return valueToTranslate.toUpperCase()
-    }, 
-    deleteEntity(entity_uuid){
+    },
+    deleteEntity(entity_uuid) {
       this.isLoading = true
 
       coreApi.glados.deleteEntity(entity_uuid)
         .then(() => {
-          
+
         })
         .catch((error) => {
           console.error(error)
@@ -153,12 +216,30 @@ export default {
           this.isLoading = false
           // maybe a bit slow to do that, deleting the selected object would be better (if response is successfull, such as 204 for example)
           this.getEntities()
+          if (this.isError)
+          {
+            this.addNewMessage("Echec de la suppression", "Une erreur est survenue lors de la suppression.", "alert")
+          }
+          else 
+          {
+            this.addNewMessage("Suppression réussie", "La suppression a bien été faite.", "success")
+          }
         })
+    },
+    addNewMessage(header, content, type) {
+      this.messages[this.message_count] = {
+        "header": header,
+        "content": content,
+        "type": type,
+        "id": this.message_count
+      }
+      this.message_count++
+    },
+    deleteMessage(message_id) {
+      delete this.messages[message_id]
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
