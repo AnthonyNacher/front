9814f8bc486f239000301dc3f2ac3f8486ec7de4
AnthonyNacher <nacheranthony@gmail.com>
2023-04-10 12:13:28 +0200

    Added a message when no entity is found

--- src/components/dashboard/Dashboard.vue
+++ src/components/dashboard/Dashboard.vue
@@ -41,6 +41,13 @@
         </tr>
       </thead>
       <tbody>
+        <tr v-if="!entities.length">
+          <td 
+            colspan="6" 
+            class="text-center">
+            Aucun appareil enregistré.
+          </td>
+        </tr>
         <tr
           class="text-center"
           v-for="entity in entities"
